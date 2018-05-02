036c9f93d56b7a8a89d82376569d7cacba258b81
Haran Kumar <hkumar8200@gmail.com>
2018-05-02 08:10:16 -0500

    Use Ace Editor

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const run = require("./src/main.js")
 
+
+const editor = ace.edit("code");
+editor.setTheme("ace/theme/chrome");
+editor.setOptions({
+    fontSize: "16px"
+})
+editor.session.setMode("ace/mode/lisp");
+
 document.getElementById("run").onclick = function () {
-    const program = document.getElementById("code").value
+    const program = editor.getValue()
 
     const output = run(program)
 
